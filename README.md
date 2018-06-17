9d06a13b04d6f70300862f9798b7ccc0b71e52ad
Huan183 <rachelqiu183@yahoo.com>
2018-06-17 14:52:06 +0800

    phaser helper to enable kinetic scrolling effect

---
+++ README.md
@@ -0,0 +1,63 @@
+## A helper function to imitate scrolling on Phaser's canvas
+
+
+
+# Inspiration
+
+ariya's [kinetic](https://github.com/ariya/kinetic/) and [littlee](https://github.com/littlee)'s scroll-related component
+
+
+
+# Features
+
+- Written in ES6
+
+- Support **either** vertical kinetic scrolling **or** horizontal kinetic scrolling( Note: Not both direction at the same time)
+
+  ​
+
+# How to Used
+1. download the 'src/phaserScrollCanvas.js' file to your project
+
+2. import the file to get the default-exported function
+
+3. Call the function to enable scrolling by passing in necessary parameters
+
+   - if intent to scroll the whole canvas, simply pass your (1) game reference, and (2) the bounds where scrolling should be active to the function can do the magic for you. For Example, as follows:
+
+   - ```javascript
+     import getScrolling from '/your/paths/to/phaserScrollCanvas.js'
+
+     // put the following in the State that you would like to do the scroll
+     getScrolling({
+       // scroll default to vertical direction, uncomment the next line if you want to scroll horizontally  
+       // direction: 'horizontal', 
+       game: window.game, // or your game reference
+       newWorldBoundsInfoArr: [0, 0, 640, 2160], // [x, y, width, height] an array that reset the World bounds. In this case, it specifies the scrollable bounds. Usually the width and height are determined by the widest and highest object you want to display.
+     })
+
+     ```
+
+   - if intent to narrow the scroll area down to a specific part on the canvas. More parameters should be passed. like this:
+
+   - ```javascript
+     import getScrolling from '/your/paths/to/phaserScrollCanvas.js'
+
+     // put the following in the State that you would like to do the scroll
+     getScrolling({
+       // scroll default to vertical direction, uncomment the next line if you want to scroll horizontally  
+       // direction: 'horizontal', 
+       game: window.game, // or your game reference
+       scrollAllBoo: false, // set to false to specify your don't want the whole canvas to scroll. Default to true.
+       newWorldBoundsInfoArr: [0, 0, 640, 2160], // [x, y, width, height] an array that reset the World bounds. Make sure this new world bounds cover your target scrolling area.
+       fixedToCameraObjsArr: [gameObjectReference [,gameObjectReference [,...]]], // an array whose items are the game objects that your want to stay put while scrolling. Default value is null, however, since you just want to enable scrolling on a specific part but the whole canvas, you should specify in the array which game object to stay still.
+       scrollObj: ObjThatCanBeScrolled, // value of scrollObj is the game object that you intent to perform the scroll
+       maskRectInfoArr: [180, 460, 380, 360] // [x, y, width, height] an array that specifies the view area of your scrollObj
+     })
+     ```
+
+
+
+# License
+
+MIT
---
+++ src/phaserScrollCanvas.js
@@ -0,0 +1,100 @@
+export default ({game, scrollAllBoo = true, newWorldBoundsInfoArr, direction = 'vertical', fixedToCameraObjsArr = null, maskRectInfoArr = [], scrollObj = null}) => {
+  let pressed, lastY, lastTrackY, vUsed, amplitude, timestamp, ticker, delta
+
+  // set a specific area to be scrolled
+  game.world.setBounds(...newWorldBoundsInfoArr)
+
+  // to scroll the whole world OR just a specific part inside the world
+  if (!scrollAllBoo && fixedToCameraObjsArr) {
+    for (let item of fixedToCameraObjsArr) {
+      item.fixedToCamera = true
+    }
+    scrollObj.mask = game.add.graphics()
+    scrollObj.mask.drawRect(...maskRectInfoArr)
+    scrollObj.mask.fixedToCamera = true
+
+    scrollObj.inputEnabled = true
+    scrollObj.events.onInputDown.add(touchScreen)
+    scrollObj.events.onInputUp.add(releasePointer)
+  } else {
+    game.input.onDown.add(touchScreen)
+    game.input.onUp.add(releasePointer)
+  }
+
+  game.input.addMoveCallback(dragOnScreen)
+
+  function touchScreen (pointerGame, pointerObj) {
+    let pointer = scrollObj ? pointerObj : pointerGame
+    pressed = true
+    lastY = lastTrackY = direction === 'vertical' ? pointer.y : pointer.x
+    vUsed = amplitude = 0
+    timestamp = Date.now()
+    ticker = setInterval(trackVelocity, 100)
+    return false
+  }
+
+  function trackVelocity () {
+    // trace and update vUsed every 100ms
+    if (!pressed) return false
+    let now, elapsed, vReal, deltaT, nowTrackY
+    now = Date.now()
+    elapsed = now - timestamp
+    nowTrackY = direction === 'vertical' ? game.input.y : game.input.x
+    deltaT = nowTrackY - lastTrackY
+    lastTrackY = nowTrackY
+    timestamp = now
+
+    vReal = 1000 * deltaT / (1 + elapsed)
+    vUsed = 0.8 * vReal + 0.2 * vUsed
+  }
+
+  function dragOnScreen (pointer) {
+    if (pressed) {
+      let nowY = direction === 'vertical' ? pointer.y : pointer.x
+      delta = lastY - nowY
+
+      if (delta > 2 || delta < -2) {
+        lastY = nowY
+
+        getScrolling(delta)
+      }
+    }
+    return false
+  }
+
+  function getScrolling (delta) {
+    if (direction === 'vertical') {
+      game.camera.y += delta
+    } else {
+      game.camera.x += delta
+    }
+  }
+
+  function releasePointer () {
+    pressed = false
+    clearInterval(ticker)
+    if (vUsed > 10 || vUsed < -10) {
+      // factor 0.1 is tweakable, fancy a 'heavy-scrolling-feel', reduce the value of factor.
+      amplitude = Math.round(0.1 * vUsed)
+      timestamp = Date.now()
+      // requestAnimationFrame only schedules a single update to the script-based animation. If subsequent animation frames are needed, then requestAnimationFrame will need to be called again from within the callback.
+      requestAnimationFrame(getAutoSlide)
+    }
+    return false
+  }
+
+  function getAutoSlide () {
+    let timeConstant = 325
+    let elapsed
+    if (amplitude) {
+      elapsed = Date.now() - timestamp
+      delta = -amplitude * Math.exp(-elapsed / timeConstant)
+      if (delta > 0.5 || delta < -0.5) {
+        getScrolling(delta)
+        requestAnimationFrame(getAutoSlide)
+      } else {
+        getScrolling(delta)
+      }
+    }
+  }
+}
